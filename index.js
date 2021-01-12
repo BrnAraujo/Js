53d70cd7355660dc7865db66a7c88b1cba1a66c9
Bruno Pereira <bruno1774@hotmail.com>
2021-01-12 16:29:55 -0300

    update 01

---
+++ index.js
@@ -0,0 +1,9 @@
+/*alert("Stardew valley"); alerta
+confirm("Gameplay elevada"); confirmação */
+prompt("what's your age?"); //pergunta
+
+/*unico sinal de igual significa "recebe"*/
+
+/* "null" nada, vazio */
+
+/*"var" usado pra criar variaveis. Tambem podermos usar "let"*/
