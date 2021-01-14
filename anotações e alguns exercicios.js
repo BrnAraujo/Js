d30f18e5d17a7663def63cab4cd71fbc77f0e46f
Bruno Pereira <bruno1774@hotmail.com>
2021-01-14 19:00:39 -0300

    att

---
+++ anotações e alguns exercicios.js
@@ -0,0 +1,30 @@
+/*alert("Stardew valley"); alerta
+confirm("Gameplay elevada"); confirmação 
+ */
+
+/*unico sinal de igual significa "recebe"*/
+
+/* "null" nada, vazio */
+
+/*"var" usado pra criar variaveis. Tambem podermos usar "let"*/
+
+ //pergunta
+    
+/*var s1 =prompt("Qual é seu nome?")
+alert ("é um prazer conhecer," + s1 +"!")*/ // "+" é concatenaçao para string e adição para Number
+
+
+/*      Exemplo de variavel com alert de soma       
+var n1 = Number(prompt("Digite um numero"))
+var n2 = Number(prompt("Digite outro numero"))
+var s = n1 + n2
+alert(`A soma dos valores ${n1} e ${n2} é igual a ${s}`)
+*/
+
+/*var n1 = Number(prompt(`digite um numero`))
+var n2 = Number(prompt(`digite outro numero`))
+ var r = n1 + n2
+document.write(`O resultado da soma de ${n1} e ${n2} é igual a ${r} !`)*/
+
+var p1 = window.document.getElementsByTagName('p')[0]
+window.document.write(`Está escrito assim: ${p1.innerText}`)
--- index.js
+++ index.js
@@ -1,26 +1,2 @@
-/*alert("Stardew valley"); alerta
-confirm("Gameplay elevada"); confirmação 
- */
-
-/*unico sinal de igual significa "recebe"*/
-
-/* "null" nada, vazio */
-
-/*"var" usado pra criar variaveis. Tambem podermos usar "let"*/
-
- //pergunta
-    
-/*var s1 =prompt("Qual é seu nome?")
-alert ("é um prazer conhecer," + s1 +"!")*/ // "+" é concatenaçao para string e adição para Number
-
-
-/*      Exemplo de variavel com alert de soma       
-var n1 = Number(prompt("Digite um numero"))
-var n2 = Number(prompt("Digite outro numero"))
-var s = n1 + n2
-alert(`A soma dos valores ${n1} e ${n2} é igual a ${s}`)
-*/
-
-var nome = prompt (`Qual é o seu nome?`)
-document.write (`seu nome tem ${nome.length} letras.`)
-document.write (`seu nome é ${nome.toUpperCase()}`)
+var bt = document.getElementById ('div#bt')
+bt.style.color = 'red'
